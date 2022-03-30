185d39aac971a324f2ed8ad1220977f09c88c4ab
HM <hieumdd@gmail.com>
2022-03-30 09:27:06 +0700

    feat: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    target: 'serverless',
     env: {
         REPO_URL: 'https://github.com/hieumdd/hieumdd',
         CV_URL: 'https://hieumdd.notion.site/Hieu-Mai-Dinh-c943b8cb36194d2cb42f2dd2af9ea517',
