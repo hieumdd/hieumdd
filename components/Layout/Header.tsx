4f23644ec9a7c48c3e09553bd9996385a8035dda
HM <hieumdd@gmail.com>
2022-04-01 21:57:31 +0700

    refactor: Refactor NavLink, Add close on click

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { FC, useState, useRef, useEffect } from 'react';
 import NextLink from 'next/link';
 import NextImage from 'next/image';
 import {
@@ -7,29 +7,11 @@ import {
     Box,
     Flex,
     Container,
-    Collapse,
-    StackDivider,
-    IconButton,
-    VStack,
-    HStack,
-    Button,
-    Icon,
     useDisclosure,
     useOutsideClick,
-    StackProps,
 } from '@chakra-ui/react';
-import { FaStream } from 'react-icons/fa';
-import { MdClose, MdMenu } from 'react-icons/md';
 
-import Socials from '../Socials';
-
-type MoblileNavLinksProps = {
-    isOpen: boolean;
-};
-
-type MobileMenuIconProps = MoblileNavLinksProps & {
-    onToggle: () => void;
-};
+import { DesktopNavLinks, MobileNavLinks, MobileMenuIcon } from './NavLinks';
 
 const Logo = () => (
     <LinkBox boxSize="40px">
@@ -39,53 +21,6 @@ const Logo = () => (
             </LinkOverlay>
         </NextLink>
     </LinkBox>
-);
-
-const BlogButton = () => (
-    <NextLink href="/articles" passHref>
-        <Button
-            className="hover-color"
-            variant="outline"
-            borderWidth="2px"
-            rightIcon={<Icon as={FaStream} fontSize="md"/>}
-            lineHeight="1em"
-            aria-label="Blog"
-        >
-            Articles
-        </Button>
-    </NextLink>
-);
-
-const DesktopNavLinks = ({ display }: StackProps) => (
-    <HStack spacing="1em" display={display}>
-        <Socials />
-        <BlogButton />
-    </HStack>
-);
-
-const MobileMenuIcon = ({ isOpen, onToggle }: MobileMenuIconProps) => (
-    <IconButton
-        display={{ base: 'flex', md: 'none' }}
-        fontSize="1.5rem"
-        onClick={onToggle}
-        icon={isOpen ? <MdClose /> : <MdMenu />}
-        aria-label="menu"
-    />
-);
-
-export const MobileNavLinks = ({ isOpen }: MoblileNavLinksProps) => (
-    <Flex
-        bgColor="inherit"
-        pb={isOpen ? '0.5rem' : 'auto'}
-        display={{ base: 'block', md: 'none' }}
-    >
-        <Collapse in={isOpen} animateOpacity>
-            <VStack align="flex-end" divider={<StackDivider />}>
-                <Socials />
-                <BlogButton />
-            </VStack>
-        </Collapse>
-    </Flex>
 );
 
 const Header = () => {
@@ -130,12 +65,10 @@ const Header = () => {
                             display={{ base: 'flex', md: 'none' }}
                         />
                         <Logo />
-                        <DesktopNavLinks
-                            display={{ base: 'none', md: 'flex' }}
-                        />
-                        <MobileMenuIcon isOpen={isOpen} onToggle={onToggle} />
+                        <DesktopNavLinks />
+                        <MobileMenuIcon isOpen={isOpen} onClick={onToggle} />
                     </Flex>
-                    <MobileNavLinks isOpen={isOpen} />
+                    <MobileNavLinks isOpen={isOpen} onClick={() => onClose()} />
                 </Flex>
             </Container>
         </Flex>
