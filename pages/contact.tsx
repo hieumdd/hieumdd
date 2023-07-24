aead547f236bfcd2fe7ca4c7006c3497290c2688
HM <hieumdd@gmail.com>
2023-07-24 19:50:11 +0700

    feat: Add calendly

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
-import { SimpleGrid } from '@chakra-ui/react';
+import { Stack } from '@chakra-ui/react';
 import { IoMdMail, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
+import { InlineWidget } from 'react-calendly';
 
 import { useSocialLinks } from '../hooks/use-social-link';
 import { SocialCard } from '../components/social-card';
@@ -9,11 +10,21 @@ const Contact: NextPage = () => {
     const { email, github, linkedin } = useSocialLinks();
 
     return (
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
-            <SocialCard label="Github" icon={IoLogoGithub} href={github} />
-            <SocialCard label="Linkedin" icon={IoLogoLinkedin} href={linkedin} />
-            <SocialCard label="Email" icon={IoMdMail} href={email} />
-        </SimpleGrid>
+        <Stack
+            direction={{ base: 'column-reverse', md: 'row' }}
+            justifyContent="space-between"
+            spacing={4}
+        >
+            <InlineWidget
+                url="https://calendly.com/hieumdd"
+                pageSettings={{ hideEventTypeDetails: true, hideGdprBanner: true }}
+            />
+            <Stack flex="1" direction="column" spacing={4}>
+                <SocialCard label="Github" icon={IoLogoGithub} href={github} />
+                <SocialCard label="Linkedin" icon={IoLogoLinkedin} href={linkedin} />
+                <SocialCard label="Email" icon={IoMdMail} href={email} />
+            </Stack>
+        </Stack>
     );
 };
 
