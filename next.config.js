0513df67871d8e2f2baa4c30113c91012550cc58
HM <hieumdd@gmail.com>
2023-07-24 21:55:05 +0700

    ci: Add gh-pages.yaml

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
+    output: 'export',
+    basePath: "/hieumdd",
     images: {
         unoptimized: true,
     },
