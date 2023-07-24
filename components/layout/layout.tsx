4ec62d1235b137155a1d89d606407c51df08bd21
HM <hieumdd@gmail.com>
2023-07-24 19:37:25 +0700

    misc: Adjust layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,5 +2,12 @@ import { Container } from '@chakra-ui/react';
 import { PropsWithChildren } from 'react';
 
 export const Layout = ({ children }: PropsWithChildren) => {
-    return <Container mt={20} >{children}</Container>;
+    return (
+        <Container
+            minH={{ base: 'calc(100vh - (80px + 88px))', md: 'calc(100vh - (160px + 88px))' }}
+            mt={{ base: 20, md: 40 }}
+        >
+            {children}
+        </Container>
+    );
 };
