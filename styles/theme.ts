24367ea0620aed8554deebbb8bebf5c416662ad2
HM <hieumdd@gmail.com>
2023-07-24 20:18:14 +0700

    feat: Update theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,24 +2,29 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
     colors: {
-        nord: {
-            50: "#FCFCFD",
-            100: "#F5F7F9",
-            200: "#EBEEF4",
-            300: "#E2E6EE",
-            400: "#D8DEE9",
-            500: "#A7B4CD",
-            600: "#798EB4",
-            700: "#516790",
-            800: "#35445F",
-            900: "#1C2331",
-            950: "#0D1017"
-        }
+        cerulean: {
+            '50': '#ecfcff',
+            '100': '#cff7fe',
+            '200': '#a5effc',
+            '300': '#67e4f9',
+            '400': '#22d0ee',
+            '500': '#06b6d4',
+            '600': '#0899b2',
+            '700': '#0e7d90',
+            '800': '#156775',
+            '900': '#165863',
+            '950': '#083b44',
+        },
     },
     components: {
         Container: {
             baseStyle: {
                 maxW: 'container.md',
+            },
+        },
+        Button: {
+            defaultProps: {
+                colorScheme: 'cerulean',
             },
         },
     },
