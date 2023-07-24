790a8ef6137c2249a90a81a60a73575530a989ed
HM <hieumdd@gmail.com>
2023-07-24 22:33:54 +0700

    feat: Add assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = (env) => ({
     reactStrictMode: true,
     output: 'export',
+    assetPrefix: env.BASE_PATH,
     basePath: env.BASE_PATH,
     images: {
         unoptimized: true,
